264dd86ed7b37183a54b5949238cfccd9d16f100
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-19 15:15:20 -0600

    Add routes to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,46 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    jobs = Job.all
+    jobs.to_json(include: { companies: { include: :hipsters } }) #eh, this may not get me the jobs of the companies that have no hispters
   end
 
+  post "/jobs" do
+    job = Job.create(
+      title: params[:title],
+      position: params[:position],
+      field: params[:field],
+      key_skill: params[:key_skill],
+      employment_type: params[:employment_type],
+      open: true, #having true here to start will work, right?
+      expired: false, #having false here to start will work, right?
+      hipster_id: nil, #having nil here to start will work, right?
+      company_id: params[:company_id] #on the frontend, somehow will need to make sure I grab the id of the company creating the job
+    )
+    job.to_json
+  end
+
+  post '/hipsters' do
+    hipster = Hipster.create(
+      name: params[:name],
+      bio: params[:bio]
+    )
+    hipster.to_json
+  end
+
+  patch '/jobs/:id' do
+    job = Job.find(params[:id])
+    job.update(
+      hipster_id: params[:hipster_id] #on the frontend, somehow will need to make sure I grab the id of the hipster being hired
+    )
+    job.to_json
+  end
+
+  delete '/jobs/:id' do
+    job = job.find(params[:id])
+    job.destroy
+    job.to_json #do I need to have this line here? Should I send a confirmation to the frontend or something?
+  end
 end
