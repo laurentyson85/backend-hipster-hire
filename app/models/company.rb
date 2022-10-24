082eb492dbcd0930ec5c2ba8aa0cd89e17df0f3a
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-24 07:04:36 -0600

    Add method to Company to count hipsters

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,9 @@
 class Company < ActiveRecord::Base
     has_many :jobs
     has_many :hipsters, through: :jobs
+
+    def hired_hipsters
+      employees = hipsters.uniq
+      "Our company employs #{employees.count} hipsters." 
+    end
   end
