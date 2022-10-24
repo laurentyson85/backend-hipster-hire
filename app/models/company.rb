e624019d125d976b1f5cc20726fb7be63cbd89cc
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-24 17:19:14 -0600

    Update hired_hipsters method in company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,9 +3,17 @@ class Company < ActiveRecord::Base
     has_many :hipsters, through: :jobs
 
     def hired_hipsters
-      employees = hipsters.uniq
-      "Our company employs #{employees.count} hipsters." 
+      employees = hipsters.uniq.count
+
+        if employees == 0 
+          "No hipsters yet! We're hiring."
+        elsif employees == 1
+          "Our company employs #{employees} hipster."
+        else  employees > 1
+          "Our company employs #{employees} hipsters."
+      end
     end
+
   end
 
   # TO DO: add hired_hipsters to controller in to_json
